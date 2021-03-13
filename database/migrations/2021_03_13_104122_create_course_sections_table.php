b764ad06ba6942b3fa083e155126bebefee97851
Andrej <zarke998@gmail.com>
2021-03-13 11:47:28 +0100

    Add course sections model and migration

---
+++ database/migrations/2021_03_13_104122_create_course_sections_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCourseSectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_sections', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+
+            $table->foreignId("course_id")->constrained()
+                ->onDelete("cascade")
+                ->onUpdate("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_sections');
+    }
+}
