4c4439bfa26acc1db658a91df16a3ae8ff6d9d69
Andrej <zarke998@gmail.com>
2021-03-17 21:27:05 +0100

    Add recommended courses

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,74 +8,44 @@
 
 @section("content")
 <div class="container-fluid paddding mb-5">
-    <div class="row mx-0">
-        @foreach($recommended_courses as $course)
-            @if($loop->first)
-                <div class="col-md-6 col-12 paddding animate-box" data-animate-effect="fadeIn">
-                    <div class="fh5co_suceefh5co_height">
-                        <img src="{{ asset($course->images[0]->src) }}" alt="img"/>
-                        <div class="fh5co_suceefh5co_height_position_absolute"></div>
-                        <div class="fh5co_suceefh5co_height_position_absolute_font">
-                            <div class="">
-                                <a href="#" class="color_fff">
-                                    <i class="fa fa-clock-o"></i>
-                                    {{ $course->release_date }}
-                                </a>
-                            </div>
-                            <div class="">
-                                <a href="../../../../../24%20News%20Free%20Website%20Template%20-%20Free-CSS.com/24-news/single.html" class="fh5co_good_font">
-                                    {{ $course->name }}
-                                </a>
+    <div class="row mx-0 intro-slider">
+        <div class="col-md-6 col-12 paddding animate-box" data-animate-effect="fadeIn">
+            <div class="fh5co_suceefh5co_height">
+                <img src="{{ asset("storage/".$recommended_courses[0]->images[0]->src) }}" alt="img"/>
+                <div class="fh5co_suceefh5co_height_position_absolute"></div>
+                <div class="fh5co_suceefh5co_height_position_absolute_font">
+                    <div class="">
+                        {{ $recommended_courses[0]->release_date }}
+                    </div>
+                    <div class="">
+                        <a href="../../../../../24%20News%20Free%20Website%20Template%20-%20Free-CSS.com/24-news/single.html" class="fh5co_good_font">
+                            {{ $recommended_courses[0]->name }}
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="row">
+                @for($i = 1; $i < $recommended_courses->count(); $i++)
+                    <div class="col-md-6 col-6 paddding animate-box" data-animate-effect="fadeIn">
+                        <div class="fh5co_suceefh5co_height_2">
+                            <img src="{{ asset("storage/".$recommended_courses[$i]->images[0]->src) }}" alt="img"/>
+                            <div class="fh5co_suceefh5co_height_position_absolute"></div>
+                            <div class="fh5co_suceefh5co_height_position_absolute_font_2">
+                                <div class="">
+                                        {{ $recommended_courses[0]->release_date }}
+                                </div>
+                                <div class=""><a href="../../../../../24%20News%20Free%20Website%20Template%20-%20Free-CSS.com/24-news/single.html" class="fh5co_good_font_2">
+                                        {{ $recommended_courses[$i]->name }}
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-{{--            @else--}}
-{{--                <div class="col-md-6">--}}
-{{--                    <div class="row">--}}
-{{--                        <div class="col-md-6 col-6 paddding animate-box" data-animate-effect="fadeIn">--}}
-{{--                            <div class="fh5co_suceefh5co_height_2"><img src="{{ asset("assets/images/science-578x362.jpg") }}" alt="img"/>--}}
-{{--                                <div class="fh5co_suceefh5co_height_position_absolute"></div>--}}
-{{--                                <div class="fh5co_suceefh5co_height_position_absolute_font_2">--}}
-{{--                                    <div class=""><a href="#" class="color_fff"> <i class="fa fa-clock-o"></i>&nbsp;&nbsp;Oct--}}
-{{--                                            28,2017 </a></div>--}}
-{{--                                    <div class=""><a href="../../../../../24%20News%20Free%20Website%20Template%20-%20Free-CSS.com/24-news/single.html" class="fh5co_good_font_2"> After all is said and done, <br>more is said than done </a></div>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        <div class="col-md-6 col-6 paddding animate-box" data-animate-effect="fadeIn">--}}
-{{--                            <div class="fh5co_suceefh5co_height_2"><img src="{{ asset("assets/images/joe-gardner-75333.jpg") }}" alt="img"/>--}}
-{{--                                <div class="fh5co_suceefh5co_height_position_absolute"></div>--}}
-{{--                                <div class="fh5co_suceefh5co_height_position_absolute_font_2">--}}
-{{--                                    <div class=""><a href="#" class="color_fff"> <i class="fa fa-clock-o"></i>&nbsp;&nbsp;Oct 28,2017 </a></div>--}}
-{{--                                    <div class=""><a href="../../../../../24%20News%20Free%20Website%20Template%20-%20Free-CSS.com/24-news/single.html" class="fh5co_good_font_2"> After all is said and done... </a></div>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        <div class="col-md-6 col-6 paddding animate-box" data-animate-effect="fadeIn">--}}
-{{--                            <div class="fh5co_suceefh5co_height_2"><img src="{{ asset("assets/images/ryan-moreno-98837.jpg") }}" alt="img"/>--}}
-{{--                                <div class="fh5co_suceefh5co_height_position_absolute"></div>--}}
-{{--                                <div class="fh5co_suceefh5co_height_position_absolute_font_2">--}}
-{{--                                    <div class=""><a href="#" class="color_fff"> <i class="fa fa-clock-o"></i>&nbsp;&nbsp;Oct--}}
-{{--                                            28,2017 </a></div>--}}
-{{--                                    <div class=""><a href="../../../../../24%20News%20Free%20Website%20Template%20-%20Free-CSS.com/24-news/single.html" class="fh5co_good_font_2"> After all is said and done, more is said than done </a></div>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        <div class="col-md-6 col-6 paddding animate-box" data-animate-effect="fadeIn">--}}
-{{--                            <div class="fh5co_suceefh5co_height_2"><img src="{{ asset("assets/images/10-1-1-875x500.jpg") }}" alt="img"/>--}}
-{{--                                <div class="fh5co_suceefh5co_height_position_absolute"></div>--}}
-{{--                                <div class="fh5co_suceefh5co_height_position_absolute_font_2">--}}
-{{--                                    <div class=""><a href="#" class="color_fff"> <i class="fa fa-clock-o"></i>&nbsp;&nbsp;Oct--}}
-{{--                                            28,2017 </a></div>--}}
-{{--                                    <div class=""><a href="../../../../../24%20News%20Free%20Website%20Template%20-%20Free-CSS.com/24-news/single.html" class="fh5co_good_font_2"> After all is said and done, more is said... </a></div>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-            @endif
-        @endforeach
+                @endfor
+            </div>
+        </div>
         <div class="intro-slider-title">
             Recommended
         </div>
