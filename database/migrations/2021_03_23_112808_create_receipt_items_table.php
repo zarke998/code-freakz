28a249df422a90f3627211d0783d1ed9712cb780
Andrej <zarke998@gmail.com>
2021-03-23 12:29:18 +0100

    Add recepits and recepit items table

---
+++ database/migrations/2021_03_23_112808_create_receipt_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReceiptItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('receipt_items', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId("receipt_id")->constrained();
+            $table->foreignId("course_id")->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('receipt_items');
+    }
+}
